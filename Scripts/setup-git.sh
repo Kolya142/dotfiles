1e772122fcf9811815ec8b69b1ecbc694982c9ca
Nikolay Shevelko <nikolasshevelkos@gmail.com>
2026-03-19 14:02:01 +0300

    Update setup-git.sh

--- Scripts/setup-git.sh
+++ Scripts/setup-git.sh
@@ -3,3 +3,4 @@ git config set --global user.email "nikolasshevelkos@gmail.com"
 # It's a publicly avaliable identifier of my key. You cannot hack me with it, lol.
 KEY=6E344F6A15BD2DCC354CD4042BB740FB6D4AE523
 gpg --list-secret-keys | grep $KEY && git config set --global user.signingkey $KEY || echo "You haven't the PGP key"
+git config --local commit.gpgsign true
